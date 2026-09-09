7689c277bcd56ff3812facead0278b2882bd4225
ADITH AK <94104632+Adithaklm@users.noreply.github.com>
2026-09-09 20:34:07 +0530

    Add customizable FFmpeg settings

--- config.py
+++ config.py
@@ -10,9 +10,18 @@ OWNER_ID = int(os.environ["OWNER_ID"])
 AUTHORIZED_USERS = {int(x) for x in os.environ.get("AUTHORIZED_USERS", "").split(",") if x.strip().isdigit()}
 AUTHORIZED_USERS.add(OWNER_ID)
 DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "/tmp/encoder")
+
+# Default FFmpeg settings. Users can override these from the bot's Settings menu.
 VIDEO_CODEC = os.environ.get("VIDEO_CODEC", "libx264")
 AUDIO_CODEC = os.environ.get("AUDIO_CODEC", "aac")
 CRF = os.environ.get("CRF", "23")
 PRESET = os.environ.get("PRESET", "veryfast")
 AUDIO_BITRATE = os.environ.get("AUDIO_BITRATE", "128k")
+VIDEO_BITRATE = os.environ.get("VIDEO_BITRATE", "")
+PIXEL_FORMAT = os.environ.get("PIXEL_FORMAT", "yuv420p")
+OUTPUT_FORMAT = os.environ.get("OUTPUT_FORMAT", "mkv")
+VIDEO_FILTER = os.environ.get("VIDEO_FILTER", "")
+AUDIO_CHANNELS = os.environ.get("AUDIO_CHANNELS", "")
+EXTRA_FFMPEG_ARGS = os.environ.get("EXTRA_FFMPEG_ARGS", "")
+
 MAX_CONCURRENT_JOBS = int(os.environ.get("MAX_CONCURRENT_JOBS", "1"))
